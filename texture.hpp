6cc8fed0c59158c150933c36cda2232d83dd7c5d
eric <eric811125@gmail.com>
2025-07-19 00:52:13 +0800

    hashed random(before perlin noise)

--- texture.hpp
+++ texture.hpp
@@ -2,6 +2,8 @@
 #define TEXTURE_H
 
 #include "utilis.hpp"
+#include "perlin.hpp"
+#include "rtw_stb_image.hpp"
 
 class texture {
   public : 
@@ -49,6 +51,39 @@ class checker_texture : public texture {
     shared_ptr<texture> odd;
 };
 
+class image_texture : public texture {
+  public:
+    image_texture(const char* filename ) : image(filename){}
+    color value(double u, double v, const point3& p) const override {
+        // If we have no texture data, then return solid cyan as a debugging aid.
+        if (image.height() <= 0) return color(0,1,1);
 
+        // Clamp input texture coordinates to [0,1] x [1,0]
+        u = interval(0,1).clamp(u);
+        v = 1.0 - interval(0,1).clamp(v);  // Flip V to image coordinates
+
+        auto i = int(u * image.width());
+        auto j = int(v * image.height());
+        auto pixel = image.pixel_data(i,j);
+
+        auto color_scale = 1.0 / 255.0;
+        return color(color_scale*pixel[0], color_scale*pixel[1], color_scale*pixel[2]);
+    }
+  private :
+    rtw_image image;
+
+};
+
+class noise_texture : public texture {
+  public:
+    noise_texture() {}
+
+    color value(double u, double v, const point3& p) const override {
+        return color(1,1,1) * noise.noise(p);
+    }
+
+  private:
+    perlin noise;
+};
 
 #endif 
